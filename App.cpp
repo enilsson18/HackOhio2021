955ba9618187a9b0e77b6853ef9dadb1bda8ff05
ChimpsterMan <chimpsterguy@gmail.com>
2021-11-06 12:54:05 -0400

    safety save before rename

--- App.cpp
+++ App.cpp
@@ -1,32 +1,50 @@
+//********************************************************* 
+// 
+// Copyright (c) Microsoft. All rights reserved. 
+// This code is licensed under the MIT License (MIT). 
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF 
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY 
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR 
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT. 
+// 
+//*********************************************************
+
 #include "pch.h"
+#include "GameMain.h"
 
-using namespace winrt;
+using namespace winrt::Windows::ApplicationModel;
+using namespace winrt::Windows::ApplicationModel::Activation;
+using namespace winrt::Windows::ApplicationModel::Core;
+using namespace winrt::Windows::Graphics::Display;
+using namespace winrt::Windows::UI::Core;
+using namespace winrt::Windows::UI::Input;
 
-using namespace Windows;
-using namespace Windows::ApplicationModel::Core;
-using namespace Windows::Foundation::Numerics;
-using namespace Windows::UI;
-using namespace Windows::UI::Core;
-using namespace Windows::UI::Composition;
-
-struct App : implements<App, IFrameworkViewSource, IFrameworkView>
+struct App : winrt::implements<App, IFrameworkViewSource, IFrameworkView>
 {
-    CompositionTarget m_target{ nullptr };
-    VisualCollection m_visuals{ nullptr };
-    Visual m_selected{ nullptr };
-    float2 m_offset{};
-
     IFrameworkView CreateView()
     {
         return *this;
     }
 
-    void Initialize(CoreApplicationView const &)
+    void Initialize(CoreApplicationView const& applicationView)
     {
+        applicationView.Activated({ this, &App::OnActivated });
+
+        CoreApplication::Suspending({ this, &App::OnSuspending });
+
+        CoreApplication::Resuming({ this, &App::OnResuming });
+
+        // At this point we have access to the device. 
+        // We can create the device-dependent resources.
+        m_deviceResources = std::make_shared<DX::DeviceResources>();
     }
 
-    void Load(hstring const&)
+    void Load(winrt::hstring const& /* entryPoint */)
     {
+        if (!m_main)
+        {
+            m_main = winrt::make_self<GameMain>(m_deviceResources);
+        }
     }
 
     void Uninitialize()
@@ -35,117 +53,121 @@ struct App : implements<App, IFrameworkViewSource, IFrameworkView>
 
     void Run()
     {
+        m_main->Run();
+    }
+
+    void SetWindow(CoreWindow const& window)
+    {
+        window.PointerCursor(CoreCursor(CoreCursorType::Arrow, 0));
+
+        PointerVisualizationSettings visualizationSettings{ PointerVisualizationSettings::GetForCurrentView() };
+        visualizationSettings.IsContactFeedbackEnabled(false);
+        visualizationSettings.IsBarrelButtonFeedbackEnabled(false);
+
+        m_deviceResources->SetWindow(window);
+
+        window.Activated({ this, &App::OnWindowActivationChanged });
+
+        window.SizeChanged({ this, &App::OnWindowSizeChanged });
+
+        window.Closed({ this, &App::OnWindowClosed });
+
+        window.VisibilityChanged({ this, &App::OnVisibilityChanged });
+
+        DisplayInformation currentDisplayInformation{ DisplayInformation::GetForCurrentView() };
+
+        currentDisplayInformation.DpiChanged({ this, &App::OnDpiChanged });
+
+        currentDisplayInformation.OrientationChanged({ this, &App::OnOrientationChanged });
+
+        currentDisplayInformation.StereoEnabledChanged({ this, &App::OnStereoEnabledChanged });
+
+        DisplayInformation::DisplayContentsInvalidated({ this, &App::OnDisplayContentsInvalidated });
+    }
+
+    void OnActivated(CoreApplicationView const& /* applicationView */, IActivatedEventArgs const& /* args */)
+    {
         CoreWindow window = CoreWindow::GetForCurrentThread();
         window.Activate();
-
-        CoreDispatcher dispatcher = window.Dispatcher();
-        dispatcher.ProcessEvents(CoreProcessEventsOption::ProcessUntilQuit);
     }
 
-    void SetWindow(CoreWindow const & window)
+    void OnVisibilityChanged(CoreWindow const& /* sender */, VisibilityChangedEventArgs const& args)
     {
-        Compositor compositor;
-        ContainerVisual root = compositor.CreateContainerVisual();
-        m_target = compositor.CreateTargetForCurrentView();
-        m_target.Root(root);
-        m_visuals = root.Children();
-
-        window.PointerPressed({ this, &App::OnPointerPressed });
-        window.PointerMoved({ this, &App::OnPointerMoved });
-
-        window.PointerReleased([&](auto && ...)
-        {
-            m_selected = nullptr;
-        });
+        m_main->Visibility(args.Visible());
     }
 
-    void OnPointerPressed(IInspectable const &, PointerEventArgs const & args)
+    void App::OnWindowActivationChanged(CoreWindow const& /* sender */, WindowActivatedEventArgs const& args)
     {
-        float2 const point = args.CurrentPoint().Position();
-
-        for (Visual visual : m_visuals)
-        {
-            float3 const offset = visual.Offset();
-            float2 const size = visual.Size();
-
-            if (point.x >= offset.x &&
-                point.x < offset.x + size.x &&
-                point.y >= offset.y &&
-                point.y < offset.y + size.y)
-            {
-                m_selected = visual;
-                m_offset.x = offset.x - point.x;
-                m_offset.y = offset.y - point.y;
-            }
-        }
-
-        if (m_selected)
-        {
-            m_visuals.Remove(m_selected);
-            m_visuals.InsertAtTop(m_selected);
-        }
-        else
-        {
-            AddVisual(point);
-        }
+        m_main->WindowActivationChanged(args.WindowActivationState());
     }
 
-    void OnPointerMoved(IInspectable const &, PointerEventArgs const & args)
+    winrt::fire_and_forget OnSuspending(IInspectable const& /* sender */, SuspendingEventArgs const& args)
     {
-        if (m_selected)
-        {
-            float2 const point = args.CurrentPoint().Position();
+        auto lifetime = get_strong();
 
-            m_selected.Offset(
-            {
-                point.x + m_offset.x,
-                point.y + m_offset.y,
-                0.0f
-            });
-        }
+        // Save app state asynchronously after requesting a deferral. Holding a deferral
+        // indicates that the application is busy performing suspending operations. Be
+        // aware that a deferral may not be held indefinitely. After about five seconds,
+        // the app will be forced to exit.
+        SuspendingDeferral deferral = args.SuspendingOperation().GetDeferral();
+
+        co_await winrt::resume_background();
+
+        m_deviceResources->Trim();
+
+        m_main->Suspend();
+
+        deferral.Complete();
     }
 
-    void AddVisual(float2 const point)
+    void OnResuming(IInspectable const& /* sender */, IInspectable const& /* args */)
     {
-        Compositor compositor = m_visuals.Compositor();
-        SpriteVisual visual = compositor.CreateSpriteVisual();
-
-        static Color colors[] =
-        {
-            { 0xDC, 0x5B, 0x9B, 0xD5 },
-            { 0xDC, 0xED, 0x7D, 0x31 },
-            { 0xDC, 0x70, 0xAD, 0x47 },
-            { 0xDC, 0xFF, 0xC0, 0x00 }
-        };
-
-        static unsigned last = 0;
-        unsigned const next = ++last % _countof(colors);
-        visual.Brush(compositor.CreateColorBrush(colors[next]));
-
-        float const BlockSize = 100.0f;
-
-        visual.Size(
-        {
-            BlockSize,
-            BlockSize
-        });
-
-        visual.Offset(
-        {
-            point.x - BlockSize / 2.0f,
-            point.y - BlockSize / 2.0f,
-            0.0f,
-        });
-
-        m_visuals.InsertAtTop(visual);
-
-        m_selected = visual;
-        m_offset.x = -BlockSize / 2.0f;
-        m_offset.y = -BlockSize / 2.0f;
+        // Restore any data or state that was unloaded on suspend. By default, data
+        // and state are persisted when resuming from suspend. Note that this event
+        // does not occur if the app was previously terminated.
+        m_main->Resume();
     }
+
+    void OnStereoEnabledChanged(DisplayInformation const& /* sender */, IInspectable const& /* args */)
+    {
+        m_deviceResources->UpdateStereoState();
+        m_main->CreateWindowSizeDependentResources();
+    }
+
+    void OnWindowSizeChanged(CoreWindow const& /* window */, WindowSizeChangedEventArgs const& args)
+    {
+        m_deviceResources->SetLogicalSize(args.Size());
+        m_main->CreateWindowSizeDependentResources();
+    }
+
+    void OnWindowClosed(CoreWindow const& /* sender */, CoreWindowEventArgs const& /* args */)
+    {
+        m_main->Close();
+    }
+
+    void OnDpiChanged(DisplayInformation const& sender, IInspectable const& /* args */)
+    {
+        m_deviceResources->SetDpi(sender.LogicalDpi());
+        m_main->CreateWindowSizeDependentResources();
+    }
+
+    void OnOrientationChanged(DisplayInformation const& sender, IInspectable const& /* args */)
+    {
+        m_deviceResources->SetCurrentOrientation(sender.CurrentOrientation());
+        m_main->CreateWindowSizeDependentResources();
+    }
+
+    void OnDisplayContentsInvalidated(DisplayInformation const& /* sender */, IInspectable const& /* args */)
+    {
+        m_deviceResources->ValidateDevice();
+    }
+
+private:
+    std::shared_ptr<DX::DeviceResources> m_deviceResources;
+    winrt::com_ptr<GameMain> m_main;
 };
 
 int __stdcall wWinMain(HINSTANCE, HINSTANCE, PWSTR, int)
 {
-    CoreApplication::Run(make<App>());
+    CoreApplication::Run(winrt::make<App>());
 }
